0935ac31642fc903047f57295d2b92919d01a867
Rickard Hillebrink <rickad.hillebrink@gmail.com>
2025-04-20 09:04:03 +0200

    first commit

---
+++ src/main/java/se/rich/space/spacePicture/SpacePictureApplication.java
@@ -0,0 +1,13 @@
+package se.rich.space.spacePicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacePictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacePictureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/rich/space/spacePicture/SpacePictureApplicationTests.java
@@ -0,0 +1,13 @@
+package se.rich.space.spacePicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacePictureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
